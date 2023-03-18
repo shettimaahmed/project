2f97ba51115347a4ee36643aad93447610ee371a
shettimaahmed <ahmedshettima29@gmail.com>
2023-03-18 10:46:15 +0100

    project

---
+++ sec.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>landing page</title>
+    <link rel="stylesheet" type="text/css" href="sec.css">
+</head>
+<body>
+
+    <div class="banner">
+       <div class="navbar">
+        <h1 class="logo">!Plastic</h1>
+           <ul>
+               <li><a href="">Home</a></li>
+               <li><a href="login.php">admin</a></li>
+               <li><a href="vendor.php">Vendor</a></li>
+               <li><a href="contact.php">Contact</a></li>
+               <li><a href="home.php">Sign In</a></li> 
+           </ul>
+       </div>  
+
+       <div class="content">
+          <h1>Plastic Management System</h1> 
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt<br>ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+ <button>learnn more</a><span></span></button>
+          <button>connect</a><span></span></button>
+       </div>
+
+
+    </div>
+
+        </div>
+<!-- <header class="hero">
+    <section>
+        <h1>We help achieve <br> your goals. </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </section>
+   <img src="images/portfolio-item13.jpg">
+        
+</header>
+
+<main>
+        <section class="container services-con">
+            <h2>Services that we can help you with</h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <section class="services">
+            <section class="service">
+                  <img src="images/s-1.png">
+                <h3>Service 1</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </section>
+            <section class="service">
+                 <img src="images/s-1.png">
+                <h3>Service 2</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </section>
+            <section class="service">
+                 <img src="images/001.png">
+                <h3>Service 3</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </section>
+            <section class="service">
+                <img src="images/006.png">
+                <h3>Service 4</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </section>
+            <section class="service">
+                <img src="images/004.png">
+                <h3>Service 5</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </section>
+            <section class="service">
+               <img src="images/003.png">
+                <h3>Service 6</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </section>
+            
+            </section>
+            
+        </section>
+    <footer class="info">
+        <section class="lands">
+            <h2>!plactic</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+        <section class="social-icon">
+            <ul>
+            <li><a href="http://images/facebook.com"> <img src="facebook.svg"></a></li>
+            <li><a href="http://images/twitter.com"> <img src="twitter.svg"></a></li>
+            <li><a href="http://images/instagram.com"> <img src="instagram.svg"></a></li>
+            </ul>
+        </section>
+        </section>
+        <section class="lands">
+            <h2 >Services</h2>
+            <hr>
+            <p>Services 1</p>
+            <p>Services 2</p>
+            <p>Services 3</p>
+            <p>Services 4</p>
+            <p>Services 5</p>
+            <p>Services 6</p>
+        </section>
+        <section class="lands">
+            <h2>Contact Us</h2>
+            <hr>
+            <p>Line 22, frontend street</p>
+            <a href="tel: +2349029754986" class="call">Click to call 09029754986</a>
+        </section>
+        <section class="copy">&copy; 2023 Ahmed Shettima Design</section>
+        
+    </footer>
+ -->
+
+
+</body>
+</html>
